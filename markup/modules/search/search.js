b2cc7da49b4b2718f8a8ae11cc8952f0452d857f
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-01-11 14:28:45 +0300

    fix auth add TZ

--- markup/modules/search/search.js
+++ markup/modules/search/search.js
@@ -11,25 +11,24 @@
       if ($scope.text){
         //console.log('submit: ', $scope.text);
         sendStat($scope.text);
-        VK.Auth.getLoginStatus(function (response) {
-            if (response.session) {
-              console.log('user: ' + response.session.mid);
-              VK.Api.call('newsfeed.search', {
-                  q: $scope.text,
-                  extended: 1
-                },
-                function (data) {
-                  console.log(data);
-                  $rootScope.searchResults = data.response;
-                  $state.go('newsList');
-                }
-              );
-
-            } else {
-              console.log('not auth');
-            }
+        VK.Api.call('newsfeed.search', {
+            q: $scope.text,
+            extended: 1
+          },
+          function (data) {
+            console.log(data);
+            $rootScope.searchResults = data.response;
+            $state.go('newsList');
           }
         );
+        //VK.Auth.getLoginStatus(function (response) {
+        //    if (response.session) {
+        //      console.log('user: ' + response.session.mid);
+        //    } else {
+        //      console.log('not auth');
+        //    }
+        //  }
+        //);
       } else {
         console.warn('submit: ', $scope.text);
       }
